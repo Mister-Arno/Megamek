aba7ae938e85858a55099664b301088dd6759fa8
Mohammed Al-Ogaili <mohammed.al-ogaili@student.uantwerpen.be>
2023-05-17 23:47:31 +0200

    [Addition] Game Event Listener for GameEnd Event
    To be used later to update ratings on GameEnd event.

---
+++ megamek/src/megamek/common/event/RatingListener.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MegaMek.
+ *
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MegaMek. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package megamek.common.event;
+
+import megamek.MegaMek;
+
+/**
+ * This listener uses the <code>GameListener</code> interface to
+ * listen for game events and make the rating system respond accordingly
+ * <p>
+ * Listens for GameEndEvents.
+ * </p>
+ *
+ * @see GameListener
+ * @see GameEvent
+ */
+public class RatingListener extends GameListenerAdapter {
+    public RatingListener() {
+        MegaMek.getLogger().info("Rating Listener connected.");
+    }
+
+    @Override
+    public void gameEnd(GameEndEvent e) {
+        // TODO: Call ranking update
+    }
+}
